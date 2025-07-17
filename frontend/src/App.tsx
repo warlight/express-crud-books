4cc244cc3bb506c84bee4ec067d5d15d2d1fe962
Davydov <roman.dav@intername.media>
2025-07-17 11:28:12 +0300

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,73 +1,43 @@
-import {Box, TextField} from '@mui/material';
 import Button from '@mui/material/Button';
-import {DataGrid, type GridColDef} from '@mui/x-data-grid';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import type {Book} from "./bookType.ts";
+import apiCalls from "./apiCalls";
+import BookForm from "./BookForm";
+import BooksList from "./BooksList";
 
 function App() {
-    const columns: GridColDef[] = [
-        {field: 'id', headerName: 'ID', width: 50, sortable: false},
-        {field: 'name', headerName: 'Book name', width: 400, sortable: false},
-        {field: 'author', headerName: 'Author', width: 350, sortable: false}
-    ];
-
     // State
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<Book[]>([]);
     const [showForm, setShowForm] = useState(false);
-    const [author, setAuthor] = useState('');
-    const [name, setName] = useState('');
     const [selectedId, setSelectedId] = useState(null);
 
-    function closeForm(): any {
-        setShowForm(false);
-        setAuthor('');
-        setName('');
-        setSelectedId(null);
-    }
-
-    async function addBook()  {
-        const book: Omit<Book, 'id'> = {
-            author: author,
-            name: name,
-        }
-
-        await fetch('http://localhost:3000/books', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(book),
-        })
-
-        closeForm();
-        await getBooks();
-    }
-
     async function getBooks() {
-        const result = await fetch('http://localhost:3000/books');
-        const rows = await result.json();
+        const rows: Book[] = await apiCalls.getBooks();
         setData(rows)
     }
 
     async function updateBookForm() {
-        const result = await fetch(`http://localhost:3000/books/${selectedId}`);
-        const selectedBook: Book = await result.json();
-        setAuthor(selectedBook.author);
-        setName(selectedBook.name);
+        if (selectedId === null) return;
+        setSelectedId(selectedId);
     }
 
     useEffect(() => {
         const fetchBooks = async () => {
             await getBooks();
         }
-
         fetchBooks()
     }, [])
+
+    useEffect(() => {
+        if (!showForm) {
+            getBooks();
+        }
+    }, [showForm]);
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="w-full max-w-4xl p-8 bg-white rounded shadow">
-                {!showForm ? (
+                {!showForm ?
                     <>
                         <Button variant="contained" onClick={() => setShowForm(!showForm)}>
                             Add book
@@ -77,7 +47,10 @@ function App() {
                                 <>
                                     <Button
                                         variant="contained"
-                                        onClick={() => {setShowForm(!showForm); updateBookForm()}}
+                                        onClick={() => {
+                                            setShowForm(!showForm);
+                                            updateBookForm()
+                                        }}
                                         sx={{'marginLeft': '8px'}}
                                     >
                                         Update book
@@ -92,70 +65,20 @@ function App() {
                             ) : (<></>)
                         }
                     </>
-                    ) : (<></>)
+                    : <></>
                 }
 
                 <div className="w-full">
                     {
-                        showForm ? (
-                            <Box
-                                display="flex"
-                                component="form"
-                                sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-                                noValidate
-                                autoComplete="off">
-
-                                <TextField
-                                    id="name"
-                                    size="small"
-                                    label="Book name"
-                                    type="text"
-                                    name="name"
-                                    variant="outlined"
-                                    onChange={(e) => setName(e.target.value)}/>
-
-                                <TextField
-                                    id="author"
-                                    size="small"
-                                    label="Author"
-                                    type="text"
-                                    name="author"
-                                    variant="outlined"
-                                    value={author}
-                                    onChange={(e) => setAuthor(e.target.value)}/>
-
-                                <Button
-                                    onClick={() => addBook()}
-                                    size="small"
-                                    variant="contained"
-                                    sx={{'marginLeft': '8px', height: '40px', alignSelf: 'center'}}>
-                                    { selectedId ? ('Update book') : ('Store book') }
-                                </Button>
-
-                                <Button
-                                    onClick={() => closeForm()}
-                                    size="small"
-                                    variant="outlined"
-                                    sx={{'marginLeft': '16px', height: '40px', alignSelf: 'center'}}>
-                                    Close form
-                                </Button>
-                            </Box>
-                        ) : (
-                            <DataGrid
-                                rows={data}
-                                columns={columns}
-                                pagination
-                                pageSizeOptions={[10]}
-                                onRowSelectionModelChange={(newSelection) => {
-                                    const firstId = Array.from(newSelection.ids)[0];
-                                    setSelectedId(firstId)
-                                }}
-                                sx={{
-                                    border: 0,
-                                    width: '100%',
-                                }}
-                            />
-                        )
+                        showForm
+                            ?
+                            <BookForm
+                                selectedId={selectedId}
+                                showForm={showForm}
+                                setSelectedId={setSelectedId}
+                                setShowForm={setShowForm}/>
+                            :
+                            <BooksList data={data} setSelectedId={setSelectedId}></BooksList>
                     }
                 </div>
             </div>
---
+++ frontend/src/apiCalls.ts
@@ -0,0 +1,33 @@
+import type {Book} from "./bookType.ts";
+
+export default {
+    async getBooks(): Promise<Book[]> {
+        const result = await fetch('http://localhost:3000/books');
+        return await result.json();
+    },
+
+    async storeBook(book: Omit<Book, 'id'>): Promise<void> {
+        await fetch('http://localhost:3000/books', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(book),
+        })
+    },
+
+    async getBook(selectedId: number): Promise<Book> {
+        const result = await fetch(`http://localhost:3000/books/${selectedId}`);
+        return await result.json();
+    },
+
+    async updateBook(book: Book): Promise<void> {
+        await fetch(`http://localhost:3000/books/${book.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(book),
+        })
+    }
+}
